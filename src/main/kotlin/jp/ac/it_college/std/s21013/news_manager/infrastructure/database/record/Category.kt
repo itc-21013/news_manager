5d8ef0d699f5e1e0ee6ae0b7fb6644c30796b9bf
s21013 <s21013@std.it-college.ac.jp>
2022-07-02 16:02:03 +0900

    application.ymlでデータベースとjacson設定

---
+++ src/main/kotlin/jp/ac/it_college/std/s21013/news_manager/infrastructure/database/record/Category.kt
@@ -0,0 +1,9 @@
+/*
+ * Auto-generated file. Created by MyBatis Generator
+ */
+package jp.ac.it_college.std.s21013.news_manager.infrastructure.database.record
+
+data class Category(
+    var id: Long? = null,
+    var name: String? = null,
+)
