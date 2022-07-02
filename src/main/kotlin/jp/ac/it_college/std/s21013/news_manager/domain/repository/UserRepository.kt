d1e38e4eaaa715f6ba5eed7f79b348527016eedd
s21013 <s21013@std.it-college.ac.jp>
2022-07-02 15:57:47 +0900

    redisまで終了

--- src/main/kotlin/jp/ac/it_college/std/s21013/news_manager/domain/repository/UserRepository.kt
+++ src/main/kotlin/jp/ac/it_college/std/s21013/news_manager/domain/repository/UserRepository.kt
@@ -1,8 +1,7 @@
 package jp.ac.it_college.std.s21013.news_manager.domain.repository
 
-import jp.ac.it_college.std.s21013.news_manager.infrastructure.database.record.UsersRecord
+import jp.ac.it_college.std.s21013.news_manager.infrastructure.database.record.Users
 
 interface UserRepository {
-    fun find(username: String) : UsersRecord?
-    fun register(user: UsersRecord)
+    fun find(email: String) : Users?
 }
